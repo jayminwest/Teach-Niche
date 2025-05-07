c56a0dd1302b010a6dd91fd6c3d71b1f93c97c85
Jaymin West <jayminwest@gmail.com>
2025-05-07 09:46:31 -0700

    checking

--- jest.config.ts
+++ jest.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
     '^.+\\.tsx?$': [
       'ts-jest',
       {
-        tsconfig: 'tsconfig.json',
+        tsconfig: 'tsconfig.test.json',
         isolatedModules: true,
       },
     ],
